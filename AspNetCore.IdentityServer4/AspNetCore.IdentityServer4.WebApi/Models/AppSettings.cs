6b8c7f28f388405340b25ca7aa81e8fcba056493
KarateJB <ppippenkimo@gmail.com>
2019-07-23 21:42:18 +0800

    Add appsettings files

---
+++ AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.IdentityServer4.WebApi.Models
+{
+    public class AppSettings
+    {
+        public HostOptions Host { get; set; }
+    }
+}
