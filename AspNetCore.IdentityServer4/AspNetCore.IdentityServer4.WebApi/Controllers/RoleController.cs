d760bac9ccb494cce2dc31a0d50853ea267311bb
JB Lin <jblin@cybersoft4u.com>
2019-09-26 18:05:09 +0800

    Create role apis

---
+++ AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Controllers/RoleController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using AspNetCore.IdentityServer4.WebApi.Models;
+using AspNetCore.IdentityServer4.WebApi.Utils;
+using JB.Infra.Service.Redis;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.IdentityServer4.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly ICacheService cache = null;
+        private readonly RedisKeyFactory cacheKeys = null;
+
+        public RoleController(
+            ICacheService cache,
+            RedisKeyFactory cacheKeys)
+        {
+            this.cache = cache;
+            this.cacheKeys = cacheKeys;
+        }
+
+        [HttpGet("Get/{userName}")]
+        public async Task<UserRole> Get([FromRoute] string userName)
+        {
+            var cacheKey = this.cacheKeys.GetKeyRoles(userName);
+            (UserRole userRole, bool isOK) = await this.cache.GetCacheAsync<UserRole>(cacheKey);
+
+            if (!isOK)
+            {
+                this.HttpContext.Response.StatusCode = (int)HttpStatusCode.NoContent;
+            }
+
+            return isOK ? userRole : null;
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> Create([FromBody] UserRole userRole)
+        {
+            var cacheKey = this.cacheKeys.GetKeyRoles(userRole.Username);
+            await this.cache.SaveCacheAsync<UserRole>(cacheKey, userRole);
+            return this.Ok();
+        }
+
+        [HttpPost("Remove/{userName}")]
+        public async Task<ActionResult> Remove([FromRoute] string userName)
+        {
+            var cacheKey = this.cacheKeys.GetKeyRoles(userName);
+            await this.cache.ClearCacheAsync(cacheKey);
+            return this.Ok();
+        }
+    }
+}
---
+++ AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Models/UserRole.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNetCore.IdentityServer4.WebApi.Models
+{
+    public class UserRole
+    {
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User roles
+        /// </summary>
+        public string Roles { get; set; }
+    }
+}
