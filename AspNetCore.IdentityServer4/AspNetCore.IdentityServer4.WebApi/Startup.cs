479b6f049d566f0b6f6a3d1d2c94c02eedbe4a2d
JB Lin <jblin@cybersoft4u.com>
2019-09-27 17:52:31 +0800

    Temp checkin

--- AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Startup.cs
+++ AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using AspNetCore.IdentityServer4.WebApi.Models;
 using AspNetCore.IdentityServer4.WebApi.Services;
 using AspNetCore.IdentityServer4.WebApi.Utils;
 using AspNetCore.IdentityServer4.WebApi.Utils.Extensions;
+using JB.Infra.Service.Redis;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -65,6 +66,8 @@ namespace AspNetCore.IdentityServer4.WebApi
             // Inject HttpClient
             services.AddHttpClient<IIdentityClient, IdentityClient>().SetHandlerLifetime(TimeSpan.FromMinutes(2)); // HttpMessageHandler lifetime = 2 min
 
+            // Inject Cache service
+            services.AddSingleton<ICacheService, RedisService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ AspNetCore.IdentityServer4/AspNetCore.IdentityServer4.WebApi/Utils/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore.IdentityServer4.WebApi.Utils.Extensions
+{
+    /// <summary>
+    /// ServiceCollections extensions
+    /// </summary>
+    public class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add Cache service
+        /// </summary>
+        /// <param name="services">IServiceCollection</param>
+        /// <returns>Self</returns>
+        public static IServiceCollection AddCacheServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ICacheKeyFactory, RedisKeyFactory>();
+            services.AddScoped<ICacheService, RedisService>();
+            return services;
+        }
+    }
+}
