129cddbb08e8afc0735cd5d5db4358574dc08196
KarateJB <ppippenkimo@gmail.com>
2020-06-03 11:23:15 +0800

    Refactor AuthMetadataUtils

--- src/AspNetCore.IdentityServer4.WebApi/Utils/AuthMetadataUtils.cs
+++ src/AspNetCore.IdentityServer4.WebApi/Utils/AuthMetadataUtils.cs
@@ -3,14 +3,24 @@ using System.Security.Authentication;
 
 namespace AspNetCore.IdentityServer4.WebApi.Utils
 {
+    /// <summary>
+    /// AuthMetadata Utility
+    /// </summary>
     public class AuthMetadataUtils
     {
+        /// <summary>
+        /// Get custom Http client handler
+        /// </summary>
+        /// <returns>HttpClientHandler</returns>
         public static HttpClientHandler GetHttpHandler()
         {
             var handler = new HttpClientHandler();
-            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-            handler.SslProtocols = SslProtocols.Tls12;
-            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual; // Optional
+            handler.SslProtocols = SslProtocols.Tls12; // Optional
+            handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            // Or write like this
+            // handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
             return handler;
         }
     }
