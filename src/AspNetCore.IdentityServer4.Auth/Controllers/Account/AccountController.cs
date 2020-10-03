3ffcee4a13d79ea239d2051b12399cd11c1a998e
KarateJB <ppippenkimo@gmail.com>
2020-10-03 21:46:22 +0800

    Create login view model

--- src/AspNetCore.IdentityServer4.Auth/Controllers/Account/AccountController.cs
+++ src/AspNetCore.IdentityServer4.Auth/Controllers/Account/AccountController.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.IdentityServer4.Auth.Models.ViewModels;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
 {
+    /// <summary>
+    /// Account controller
+    /// </summary>
     [AllowAnonymous]
     public class AccountController : ControllerBase
     {
+        private readonly ILogger<AccountController> logger = null;
+        private readonly IIdentityServerInteractionService interaction = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="interaction">Idsrc interaction service</param>
+        public AccountController(
+            ILogger<AccountController> logger,
+            IIdentityServerInteractionService interaction)
+        {
+            this.logger = logger;
+            this.interaction = interaction;
+        }
         /// <summary>
         /// Entry point into the login workflow
         /// </summary>
@@ -17,8 +38,9 @@ namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
         public async Task<IActionResult> Login(string returnUrl)
         {
             throw new NotImplementedException();
+
             // build a model so we know what to show on the login page
-            //// var vm = await BuildLoginViewModelAsync(returnUrl);
+            LoginViewModel vm = await this.buildLoginViewModelAsync(returnUrl);
 
             ////if (vm.IsExternalLoginOnly)
             ////{
@@ -27,6 +49,11 @@ namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
             ////}
 
             ////return View(vm);
+        }
+
+        private async Task<LoginViewModel> buildLoginViewModelAsync(string returnUrl)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/AspNetCore.IdentityServer4.Auth/Models/ViewModels/LoginInputModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore.IdentityServer4.Auth.Models.ViewModels
+{
+    /// <summary>
+    /// LoginInputModel
+    /// </summary>
+    public class LoginInputModel
+    {
+        /// <summary>
+        /// User name
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Pwd
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Is remember login
+        /// </summary>
+        public bool RememberLogin { get; set; }
+
+        /// <summary>
+        /// Return url
+        /// </summary>
+        public string ReturnUrl { get; set; }
+    }
+}
