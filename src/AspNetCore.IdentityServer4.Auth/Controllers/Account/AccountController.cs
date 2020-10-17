04d73d76d776620bda658688e21fce6c9c6dce9e
KarateJB <ppippenkimo@gmail.com>
2020-10-17 14:32:02 +0800

    Add LoginViewModel init method

--- src/AspNetCore.IdentityServer4.Auth/Controllers/Account/AccountController.cs
+++ src/AspNetCore.IdentityServer4.Auth/Controllers/Account/AccountController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.IdentityServer4.Auth.Models;
 using AspNetCore.IdentityServer4.Auth.Models.ViewModels;
+using IdentityServer4;
 using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,22 +17,26 @@ namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
     /// Account controller
     /// </summary>
     [AllowAnonymous]
-    public class AccountController : ControllerBase
+    public class AccountController : Controller
     {
         private readonly ILogger<AccountController> logger = null;
         private readonly IIdentityServerInteractionService interaction = null;
+        private readonly IAuthenticationSchemeProvider schemeProvider = null;
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="logger">Logger</param>
         /// <param name="interaction">Idsrc interaction service</param>
+        /// <param name="schemeProvider">Authentication scheme provider</param>
         public AccountController(
             ILogger<AccountController> logger,
-            IIdentityServerInteractionService interaction)
+            IIdentityServerInteractionService interaction,
+            IAuthenticationSchemeProvider schemeProvider)
         {
             this.logger = logger;
             this.interaction = interaction;
+            this.schemeProvider = schemeProvider;
         }
         /// <summary>
         /// Entry point into the login workflow
@@ -37,8 +44,6 @@ namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
         [HttpGet]
         public async Task<IActionResult> Login(string returnUrl)
         {
-            throw new NotImplementedException();
-
             // build a model so we know what to show on the login page
             LoginViewModel vm = await this.buildLoginViewModelAsync(returnUrl);
 
@@ -48,12 +53,65 @@ namespace AspNetCore.IdentityServer4.Auth.Controllers.Account
             ////    return RedirectToAction("Challenge", "External", new { provider = vm.ExternalLoginScheme, returnUrl });
             ////}
 
-            ////return View(vm);
+            return View(vm);
         }
 
         private async Task<LoginViewModel> buildLoginViewModelAsync(string returnUrl)
         {
-            throw new NotImplementedException();
+            var context = await this.interaction.GetAuthorizationContextAsync(returnUrl);
+            ////if (context?.IdP != null)
+            ////{
+            ////    var isLocal = context.IdP == IdentityServerConstants.LocalIdentityProvider;
+
+            ////    // this is meant to short circuit the UI and only trigger the one external IdP
+            ////    var vm = new LoginViewModel
+            ////    {
+            ////        EnableLocalLogin = isLocal,
+            ////        ReturnUrl = returnUrl,
+            ////        Username = context?.LoginHint,
+            ////    };
+
+            ////    if (!isLocal)
+            ////    {
+            ////        vm.ExternalProviders = new[] { new ExternalProvider { AuthenticationScheme = context.IdP } };
+            ////    }
+
+            ////    return vm;
+            ////}
+
+            var schemes = await this.schemeProvider.GetAllSchemesAsync();
+
+            var providers = schemes
+                .Where(x => x.DisplayName != null)
+                .Select(x => new ExternalProvider
+                {
+                    DisplayName = x.DisplayName,
+                    AuthenticationScheme = x.Name
+                }).ToList();
+
+            var allowLocal = true;
+            if (context?.ClientId != null)
+            {
+                var client = await _clientStore.FindEnabledClientByIdAsync(context.ClientId);
+                if (client != null)
+                {
+                    allowLocal = client.EnableLocalLogin;
+
+                    ////if (client.IdentityProviderRestrictions != null && client.IdentityProviderRestrictions.Any())
+                    ////{
+                    ////    providers = providers.Where(provider => client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
+                    ////}
+                }
+            }
+
+            return new LoginViewModel
+            {
+                AllowRememberLogin = AccountOptions.AllowRememberLogin,
+                EnableLocalLogin = allowLocal && AccountOptions.AllowLocalLogin,
+                ReturnUrl = returnUrl,
+                Username = context?.LoginHint,
+                ExternalProviders = providers.ToArray()
+            };
         }
     }
 }
---
+++ src/AspNetCore.IdentityServer4.Auth/Models/AccountOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AspNetCore.IdentityServer4.Auth.Models
+{
+    /// <summary>
+    /// Account options
+    /// </summary>
+    public class AccountOptions
+    {
+        public static bool AllowLocalLogin = true;
+        public static bool AllowRememberLogin = true;
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+
+        public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+    }
+}
