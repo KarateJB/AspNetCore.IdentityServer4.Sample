d865b09e1d91ed13f701dbc39245de036467a0a0
KarateJB <ppippenkimo@gmail.com>
2021-04-09 01:01:16 +0800

    Fix typo

--- src/AspNetCore.IdentityServer4.WebApi/wwwroot/js/app-config.js
+++ src/AspNetCore.IdentityServer4.WebApi/wwwroot/js/app-config.js
@@ -1,2 +1,2 @@
-﻿export const CLIENT_HOST_URL = "https://localhost:5001";
-export const AUTH_HOST_URL = "https://localhost:6001";
+﻿export const AUTH_HOST_URL = "https://localhost:6001";
+export const CLIENT_HOST_URL = "https://localhost:5001";
