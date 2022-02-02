c43daf91d17f5561badd952afd267a2c632cc431
karatejb <ppippenkimo@gmail.com>
2022-02-03 00:38:09 +0800

    Create middleware

--- src/AspNetCore.Prometheus/Startup.cs
+++ src/AspNetCore.Prometheus/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.Prometheus.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -45,6 +46,9 @@ namespace AspNetCore.Prometheus
 
             // Prometheus
             app.UseMetricServer();
+
+            // Custom Middleware
+            app.UseRequestCounter();
 
             app.UseEndpoints(endpoints =>
             {
